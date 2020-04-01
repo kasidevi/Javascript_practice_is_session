46dbf14998c7a532ade7cc66047b21b70514376e
kasidevi <58264441+kasidevi@users.noreply.github.com>
2020-04-01 20:17:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,16 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { CarList } from './components/CarsList/index.js';
-import './components/CarsList/carlist.css';
-import { TodoList } from './components/TodoList/todolist.js';
-import './components/TodoList/todolist.css';
-import { FormComponent } from './components/FormComponents/FormComponent.js';
-import './components/FormComponents/FormComponent.css';
-import { CountriesDashboardApp } from './components/CountriesDashboardApp/CountriesDashboardApp.js';
-import CountryDetails from './components/CountryDetails/CountryDetails.js';
-import './components/CountriesDashboardApp/CountriesDashboardApp.css';
-
 import HomePage from "./components/HomePage";
 import Page1 from "./components/Page1";
 
